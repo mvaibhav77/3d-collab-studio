66e06dbc4544685a83bddfa4ba7e1120fec81e20
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2025-08-10 10:09:55 +0530

    fix: update socket connection URL to use environment variable for API base URL

--- apps/mfe-toolbar/src/socket.ts
+++ apps/mfe-toolbar/src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
 // Connect to the API server we created
-const URL = "http://localhost:3001";
+const URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 export const socket = io(URL);
